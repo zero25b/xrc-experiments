a3f78a8aabd02b37c4a166535718ffa228c35a40
Zero25 <zero25@tutanota.com>
2023-01-01 03:12:27 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="xrc-experiments",
+    version="0.0.1",
+    packages=["tests", "xrc_utils", "xrc_experiments", "xrc_simulations"],
+    url="",
+    license="MIT",
+    author="zero25",
+    author_email="zero25@tutanota.com",
+    description="Tools and helper functions for analyzing xRhodium blockchain data",
+)
